58fb4440b78c5fe0f1443196cb051b22036d192a
mygithubangebenie <angebenie23@gmail.com>
2022-03-26 14:27:58 +0200

    first commit to index

---
+++ src/main/java/com/example/BlogRepository.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Blog, Integer>{
+
+	
+
+}
---
+++ src/main/java/com/example/Blogapp1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blogapp1Application.class, args);
+	}
+
+}
